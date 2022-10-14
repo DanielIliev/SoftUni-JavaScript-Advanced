6797e52391a6b10019d432d69f31d5e13d034ab4
DanielIliev <daniel.iliev.didiencho@gmail.com>
2022-10-14 12:13:59 +0300

    Mid term practice task completed

--- MidTermRetake2.js
+++ MidTermRetake2.js
@@ -11,7 +11,7 @@ function bonusSystem(entries) {
         console.log(`Max Bonus: 0.\nThe student has attended 0 lectures.`);
         return;
     }
-    
+
     for (let index = 3; index < entries.length; index++) {
         studentsAttendance.push(entries[index]);
     }
@@ -50,9 +50,70 @@ function bonusSystem(entries) {
 //     '12', '19', '24',
 //     '16', '20'
 // ]);
-bonusSystem([
-    '10', '30', '14', '8',
-    '23', '27', '28', '15',
-    '17', '25', '26', '5',
-    '18'
-]);
+// bonusSystem([
+//     '10', '30', '14', '8',
+//     '23', '27', '28', '15',
+//     '17', '25', '26', '5',
+//     '18'
+// ]);
+
+function muOnline(commands) {
+    // Set the initial values of the hero
+    let health = 100;
+    let bitcoins = 0;
+    let room = 0;
+
+    let actionsArray = commands.split('|');
+    for (let index = 0; index < actionsArray.length; index++) {
+        let action = processAction(actionsArray[index].split(' '), health, bitcoins);
+        health = action[0];
+        bitcoins = action[1];
+        room++;
+        if (health <= 0) {
+            break;
+        }
+    }
+
+    if (health <= 0) {
+        console.log(`Best room: ${room}`);
+    } else {
+        console.log(`You've made it!\nBitcoins: ${bitcoins}\nHealth: ${health}`);
+    }
+
+    function processAction(actionArray, currentHealth, currentBitcoins) {
+        let actionType = actionArray[0];
+        let actionValue = Number(actionArray[1]);
+        let heroStatus = [currentHealth, currentBitcoins];
+
+        switch (actionType) {
+            case 'potion':
+                let healingValue = actionValue;
+                if (currentHealth + healingValue < 100) {
+                    heroStatus[0] += healingValue;
+                } else {
+                    healingValue = (currentHealth-100)*-1;
+                    heroStatus[0] += healingValue;
+                }
+                console.log(`You healed for ${healingValue} hp.\nCurrent health: ${heroStatus[0]} hp.`)
+                break;
+            case 'chest':
+                heroStatus[1] += actionValue;
+                console.log(`You found ${actionValue} bitcoins.`);
+                break;
+            default:
+                heroStatus[0] -= actionValue;
+                if (heroStatus[0] <= 0) {
+                    console.log(`You died! Killed by ${actionType}.`);
+                } else {
+                    console.log(`You slayed ${actionType}.`);
+                }
+                break;
+        }
+
+        return heroStatus;
+    }
+
+}
+
+// muOnline("rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000");
+muOnline("cat 10|potion 30|orc 10|chest 10|snake 25|chest 110");
