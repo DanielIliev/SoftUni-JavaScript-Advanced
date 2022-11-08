c185db057084520d8fd11d4a47628f2e93f17eac
DanielIliev <daniel.iliev.didiencho@gmail.com>
2022-11-08 22:14:24 +0200

    tests

--- FinalExamPractice2.js
+++ FinalExamPractice2.js
@@ -1,7 +1,36 @@
-function worldTour(entries) {
-    let stops = entries.shift();
+function worldTour(commands) {
+    let stops = commands.shift();
+
+    Travel:
+    for (const command of commands) {
+        let currentCommand = command.split(' ');
+        switch (currentCommand[0]) {
+            case 'Add':
+                let [notUsed, index, dest] = currentCommand[1].split(':');
+                stops = addDestination(index, dest, stops);
+            break;
+            case 'Remove':
+
+            break;
+            case 'Travel':
+            break Travel;
+            default:
+                if (currentCommand.includes('Switch')) {
+                    console.log('Switch');
+                }
+            break;
+        }
+    }
 
     console.log(stops);
+
+    function addDestination(index, destination, destinations) {
+        let tempString = '';
+        if (destinations[index]) {
+            tempString = destinations.slice(0, index) + destination + destinations.slice(index);
+        }
+        return tempString;
+    }
 }
 worldTour(([
     "Hawai::Cyprys-Greece",
@@ -10,3 +39,12 @@ worldTour(([
     "Switch:Hawai:Bulgaria",
     "Travel"
 ]));
+
+// "Add Stop:{index}:{string}":
+// Insert the given string at that index only if the index is valid
+
+// "Remove Stop:{start_index}:{end_index}":
+// Remove the elements of the string from the starting index to the end index (inclusive) if both indices are valid
+
+// "Switch:{old_string}:{new_string}":
+// If the old string is in the initial string, replace it with the new one (all occurrences)
