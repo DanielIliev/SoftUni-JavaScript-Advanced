58b663d626730706a8d93838b16ff1cc845360d5
DanielIliev <daniel.iliev.didiencho@gmail.com>
2022-11-08 23:18:11 +0200

    final exam practice task completed

--- FinalExamPractice2.js
+++ FinalExamPractice2.js
@@ -68,3 +68,29 @@ function worldTour(commands) {
 // If the old string is in the initial string, replace it with the new one (all occurrences)
 
 // Hawai::RomeCyprys-Greece
+
+function destinationMapper(entryString) {
+
+    let pattern = new RegExp(/=[A-Z]{1}[A-Za-z]{2,}=|\/[A-Z]{1}[A-Za-z]{2,}\//, 'gi');
+    let destinationsInfo = entryString.toString().match(pattern);
+    let destinationArray = [];
+
+    if (destinationsInfo) {
+        for (const destination of destinationsInfo) {
+            if (destination.includes('=')) {
+                destinationArray.push(destination.split('=')[1]);
+            } else if (destination.includes('/')) {
+                destinationArray.push(destination.split('/')[1]);
+            }
+        }
+    }
+
+    console.log(`Destinations: ${destinationArray.join(', ')}`);
+
+    let destinationPoints = destinationArray.join('').length;
+
+    console.log(`Travel Points: ${destinationPoints}`);
+
+}
+destinationMapper('=Hawai=/Cyprus/=Invalid/invalid==i5valid=/I5valid/=i=');
+destinationMapper('ThisIs some InvalidInput');
