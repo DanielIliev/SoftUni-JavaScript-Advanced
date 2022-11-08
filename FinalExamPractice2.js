666abdca2743ee300d9034e8fbb750c4c3e4e907
DanielIliev <daniel.iliev.didiencho@gmail.com>
2022-11-08 22:50:06 +0200

    Final exam practice task completed

--- FinalExamPractice2.js
+++ FinalExamPractice2.js
@@ -3,42 +3,60 @@ function worldTour(commands) {
 
     Travel:
     for (const command of commands) {
-        let currentCommand = command.split(' ');
-        switch (currentCommand[0]) {
-            case 'Add':
-                let [notUsed, index, dest] = currentCommand[1].split(':');
-                stops = addDestination(index, dest, stops);
-            break;
-            case 'Remove':
+        let currentCommand = command.split(':');
 
-            break;
+        switch (currentCommand[0]) {
+            case 'Add Stop':
+                stops = addDestination(currentCommand[1], currentCommand[2], stops);
+                break;
+            case 'Remove Stop':
+                let startId = Number(currentCommand[1]);
+                let endId = Number(currentCommand[2]);
+                stops = removeDestination(startId, endId, stops);
+                break;
+            case 'Switch':
+                let pattern = new RegExp(currentCommand[1], 'gi');
+                stops = stops.replace(pattern, currentCommand[2]);
+                break;
             case 'Travel':
-            break Travel;
-            default:
-                if (currentCommand.includes('Switch')) {
-                    console.log('Switch');
-                }
-            break;
+                break Travel;
         }
+        console.log(stops);
     }
 
-    console.log(stops);
+    console.log(`Ready for world tour! Planned stops: ${stops}`);
 
     function addDestination(index, destination, destinations) {
         let tempString = '';
+
         if (destinations[index]) {
             tempString = destinations.slice(0, index) + destination + destinations.slice(index);
         }
+
         return tempString;
     }
+
+    function removeDestination(startId, endId, destinations) {
+        let tempString = destinations;
+        let temp = '';
+
+        if (destinations[startId] && destinations[endId]) {
+            temp = destinations.slice(startId, endId + 1);
+        }
+
+        tempString = tempString.replace(temp, '');
+
+        return tempString;
+    }
+
 }
-worldTour(([
-    "Hawai::Cyprys-Greece",
-    "Add Stop:7:Rome",
-    "Remove Stop:11:16",
-    "Switch:Hawai:Bulgaria",
-    "Travel"
-]));
+// worldTour(["Hawai::Cyprys-Greece",
+// "Add Stop:7:Rome",
+// "Remove Stop:11:16",
+// "Switch:Hawai:Bulgaria",
+// "Travel"]);
+
+
 
 // "Add Stop:{index}:{string}":
 // Insert the given string at that index only if the index is valid
@@ -48,3 +66,5 @@ worldTour(([
 
 // "Switch:{old_string}:{new_string}":
 // If the old string is in the initial string, replace it with the new one (all occurrences)
+
+// Hawai::RomeCyprys-Greece
