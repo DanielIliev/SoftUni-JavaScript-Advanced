a4cfd14595d6c454b518bb173bc3ee825242f384
DanielIliev <daniel.iliev.didiencho@gmail.com>
2022-12-03 11:21:28 +0200

    Fixed the code to pass the failing tests

--- FinalsPractices/Exam4/heroesOfCodeAndLogic.js
+++ FinalsPractices/Exam4/heroesOfCodeAndLogic.js
@@ -1,110 +1,110 @@
-// 80/100
-function heroesOfCodeAndLogic(entries) {
-    // Variables declaration
-    let heroesCount = Number(entries.shift());
-    let heroes = {};
+// 100/100
+function heroesOfCode(entries) {
+  let heroesCount = Number(entries.shift());
+  let heroes = {};
 
-    for (let index = 0; index < heroesCount; index++) {
-        let [heroName, heroHealth, heroMana] = entries.shift().split(' ');
-        heroes[heroName] = generateHeroStats(Number(heroHealth), Number(heroMana));
+  for (let i = 0; i < heroesCount; i++) {
+    let [heroName, heroHealth, heroMana] = entries.shift().split(' ');
+
+    heroes[heroName] = {'health': Number(heroHealth), 'mana': Number(heroMana)};
+  }
+
+  let commands = entries.slice(0, entries.indexOf('End'));
+
+  for (command of commands) {
+    let [commandType, heroName, value1, value2] = command.split(' - ');
+
+    switch (commandType) {
+      case 'Heal':
+        let health = Number(value1);
+
+        heroes = healHero(heroes, heroName, health);
+        break;
+      case 'Recharge':
+        let mana = Number(value1);
+
+        heroes = rechargeHero(heroes, heroName, mana);
+        break;
+      case 'TakeDamage':
+        let damage = Number(value1);
+        let attacker = value2;
+
+        heroes = damageHero(heroes, heroName, attacker, damage);
+        break;
+      case 'CastSpell':
+        let manaCost = Number(value1);
+        let spell = value2;
+
+        heroes = castSpell(heroes, heroName, spell, manaCost);
+        break;
+      default:
+        break;
+    }
+  }
+
+  // Print the heroes left
+  for (let [hero, stats] of Object.entries(heroes)) {
+    console.log(`${hero}\n  HP: ${stats.health}\n  MP: ${stats.mana}`);
+  }
+
+  function healHero(heroes, heroName, healthAdded) {
+    let temp = heroes[heroName].health + healthAdded;
+
+    if (temp > 100) {
+        console.log(`${heroName} healed for ${100 - heroes[heroName].health} HP!`);
+        heroes[heroName].health = 100;
+    } else {
+        console.log(`${heroName} healed for ${healthAdded} HP!`);
+        heroes[heroName].health += healthAdded;
     }
 
-    // Process the received commands
-    let commands = entries.slice(0, entries.indexOf('End'));
+    return heroes;
+  }
 
-    for (const command of commands) {
-        let [commandType, heroName, value1, value2] = command.split(' - ');
+  function rechargeHero(heroes, heroName, manaAdded) {
+    let temp = heroes[heroName].mana + manaAdded;
 
-        switch (commandType) {
-            case 'CastSpell':
-                heroes = castSpell(heroes, heroName, Number(value1), value2);
-                break;
-            case 'TakeDamage':
-                heroes = takeDamage(heroes, heroName, Number(value1), value2);
-                break;
-            case 'Recharge':
-                heroes = rechargeHero(heroes, heroName, Number(value1));
-                break;
-            case 'Heal':
-                heroes = healHero(heroes, heroName, Number(value1));
-                break;
-
-            default:
-                break;
-        }
+    if (temp > 200) {
+        console.log(`${heroName} recharged for ${200 - heroes[heroName].mana} MP!`);
+        heroes[heroName].mana = 200;
+    } else {
+        console.log(`${heroName} recharged for ${manaAdded} MP!`);
+        heroes[heroName].mana += manaAdded;
     }
 
-    // Print the heroes left in the party in the required format
-    for (const hero in heroes) {
-        if (Object.hasOwnProperty.call(heroes, hero)) {
-            const element = heroes[hero];
-            console.log(`${hero}\n  HP: ${element.health}\n  MP: ${element.mana}`);
-        }
+    return heroes;
+  }
+
+  function damageHero(heroes, heroName, attackerName, damageReceived) {
+    let damage = Number(damageReceived);
+
+    if ((heroes[heroName].health - damage) <= 0) {
+      console.log(`${heroName} has been killed by ${attackerName}!`);
+      delete(heroes[heroName]);
+    } else {
+      heroes[heroName].health -= damage;
+      console.log(`${heroName} was hit for ${damage} HP by ${attackerName} and now has ${heroes[heroName].health} HP left!`);
     }
 
-    function generateHeroStats(health, mana) {
-        return {
-            health,
-            mana
-        }
+    return heroes;
+  }
+
+  function castSpell(heroes, heroName, spellName, manaSpent) {
+    let mana = manaSpent;
+
+    if ((heroes[heroName].mana - mana) < 0) {
+      console.log(`${heroName} does not have enough MP to cast ${spellName}!`);
+    } else {
+      heroes[heroName].mana -= mana;
+      console.log(`${heroName} has successfully cast ${spellName} and now has ${heroes[heroName].mana} MP!`);
     }
 
-    function takeDamage(heroes, hero, damageValue, attackerName) {
-        let temp = heroes[hero].health - damageValue;
-
-        if (temp < 0) {
-            console.log(`${hero} has been killed by ${attackerName}!`);
-            delete(heroes[hero]);
-        } else {
-            heroes[hero].health -= damageValue;
-            console.log(`${hero} was hit for ${damageValue} HP by ${attackerName} and now has ${heroes[hero].health} HP left!`);
-        }
-
-        return heroes;
-    }
-
-    function castSpell(heroes, hero, manaCost, spellName) {
-        let temp = heroes[hero].mana - manaCost;
-
-        if (temp < 0) {
-            console.log(`${hero} does not have enough MP to cast ${spellName}!`);
-        } else {
-            heroes[hero].mana -= manaCost;
-            console.log(`${hero} has successfully cast ${spellName} and now has ${heroes[hero].mana} MP!`);
-        }
-
-        return heroes;
-    }
-
-    function healHero(heroes, hero, healthValue) {
-        let temp = heroes[hero].health + healthValue;
-
-        if (temp > 100) {
-            console.log(`${hero} healed for ${100 - heroes[hero].health} HP!`);
-            heroes[hero].health = 100;
-        } else {
-            console.log(`${hero} healed for ${healthValue} HP!`);
-            heroes[hero].health += healthValue;
-        }
-
-        return heroes;
-    }
-
-    function rechargeHero(heroes, hero, manaValue) {
-        let temp = heroes[hero].mana + manaValue;
-
-        if (temp > 200) {
-            console.log(`${hero} recharged for ${200 - heroes[hero].mana} MP!`);
-            heroes[hero].mana = 200;
-        } else {
-            console.log(`${hero} recharged for ${manaValue} MP!`);
-            heroes[hero].mana += manaValue;
-        }
-
-        return heroes;
-    }
+    return heroes;
+  }
 }
-// heroesOfCodeAndLogic([
+
+// heroesOfCode(
+//   [
 //     '2',
 //     'Solmyr 85 120',
 //     'Kyrre 99 50',
@@ -112,45 +112,22 @@ function heroesOfCodeAndLogic(entries) {
 //     'Recharge - Solmyr - 50',
 //     'TakeDamage - Kyrre - 66 - Orc',
 //     'CastSpell - Kyrre - 15 - ViewEarth',
-//     'End'
-// ]);
+//     'End',
+//   ]
+// );
 
-heroesOfCodeAndLogic([
+heroesOfCode(
+  [
     '4',
-'Adela 90 150',
-'SirMullich 70 40',
-'Ivor 1 111',
-'Tyris 94 61',
-'Heal - SirMullich - 50',
-'Recharge - Adela - 100',
-'CastSpell - Tyris - 1000 - Fireball',
-'TakeDamage - Tyris - 99 - Fireball',
-'TakeDamage - Ivor - 3 - Mosquito',
-'End'
-])
-
-// Instructions / TODOs
-
-// There are several actions that the heroes can perform:
-
-// "CastSpell – {hero name} – {MP needed} – {spell name}" - DONE
-// If the hero has the required MP, he casts the spell, thus reducing his MP. Print this message:
-// "{hero name} has successfully cast {spell name} and now has {mana points left} MP!"
-// If the hero is unable to cast the spell print:
-// "{hero name} does not have enough MP to cast {spell name}!"
-
-// "TakeDamage – {hero name} – {damage} – {attacker}" - DONE
-// Reduce the hero HP by the given damage amount. If the hero is still alive (his HP is greater than 0) print:
-// "{hero name} was hit for {damage} HP by {attacker} and now has {current HP} HP left!"
-// If the hero has died, remove him from your party and print:
-// "{hero name} has been killed by {attacker}!"
-
-// "Recharge – {hero name} – {amount}" - DONE
-// The hero increases his MP. If it brings the MP of the hero above the maximum value (200), MP is increased to 200. (the MP can't go over the maximum value).
-//  Print the following message:
-// "{hero name} recharged for {amount recovered} MP!"
-
-// "Heal – {hero name} – {amount}" - DONE
-// The hero increases his HP. If a command is given that would bring the HP of the hero above the maximum value (100), HP is increased to 100 (the HP can't go over the maximum value).
-//  Print the following message:
-// "{hero name} healed for {amount recovered} HP!"
+    'Adela 90 150',
+    'SirMullich 70 40',
+    'Ivor 1 111',
+    'Tyris 94 61',
+    'Heal - SirMullich - 50',
+    'Recharge - Adela - 100',
+    'CastSpell - Tyris - 1000 - Fireball',
+    'TakeDamage - Tyris - 99 - Fireball',
+    'TakeDamage - Ivor - 3 - Mosquito',
+    'End',
+  ]
+)
