d2bf2fd38b59d4228c5287c3085b72ac72f8d728
DanielIliev <daniel.iliev.didiencho@gmail.com>
2022-12-22 11:49:43 +0200

    Still in progress

---
+++ ObjectsAndComposition/lowestPrice.js
@@ -0,0 +1,21 @@
+// Still in progress
+function lowestPrices(products) {
+    let cities = {};
+
+    for (const product of products) {
+        let [town, productName, productPrice] = product.split(' | ');
+             
+    }
+
+    
+}
+
+lowestPrices([
+    'Sample Town | Sample Product | 1000',
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10'
+]);
---
+++ SyntaxFunctionsStatements/timeToTravel.js
@@ -0,0 +1,10 @@
+function timeToTravel(steps, distancePerStep, speed) {
+    let stepsDistance = Number(steps);
+    let stepSize = Number(distancePerStep);
+    let speedHrs = Number(speed);
+    let distanceInMeters = stepsDistance * stepSize;
+
+    
+
+}
+timeToTravel(4000, 0.60, 5);
