0bb1d496b9cd5ce70011de8111626105acdb73af
DanielIliev <daniel.iliev.didiencho@gmail.com>
2022-12-23 03:10:49 +0200

    In progress

--- ObjectsAndComposition/townsJSON.js
+++ ObjectsAndComposition/townsJSON.js
@@ -1,12 +1,38 @@
+// 33/100 more work is required
 function towns(townsInfo) {
-    let towns = {};
+    // Variable declarations
+    let towns = [];
+
+    // Discarding the first element of the input as it's not required for the functionality
     townsInfo.shift();
 
-    console.log(townsInfo);
+    let townInfoPattern = new RegExp(/\| (?<town>[A-Za-z -]+) \| (?<latitude>\d+\.?\d*) \| (?<longitude>\d+\.?\d*) \|/);
+
+    for (const town of townsInfo) {
+        let townCheck = townInfoPattern.exec(town);
+        if (townCheck) {
+            let [name, latitude, longitude] = [townCheck[1], townCheck[2], townCheck[3]];
+
+            latitude = Math.round(latitude * 100) / 100;
+            longitude = Math.round(longitude * 100) / 100;
+            towns.push(createTownObject(name, latitude, longitude));
+        }
+    }
+
+    console.log(JSON.stringify(towns));
+
+    // Generate town object
+    function createTownObject(name, latitude, longitude) {
+        return {
+            Town: name,
+            Latitude: latitude,
+            Longitude: longitude
+        }
+    }
 }
 
 towns([
     '| Town | Latitude | Longitude |',
-    '| Sofia | 42.696552 | 23.32601 |',
+    '| Veliko Petrohansko-Hanche | 42.696552 | 23.32601 |',
     '| Beijing | 39.913818 | 116.363625 |'
 ]);
