d02d20eaabc7c665186cc4aa4f5573415fd63bdc
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-01-12 11:19:08 +0200

    Removed redundant else condition

--- ObjectsAndComposition/constructionCrew.js
+++ ObjectsAndComposition/constructionCrew.js
@@ -1,9 +1,7 @@
 function constructionCrew(workerObject) {
-    if (workerObject.dizziness == true) {
-      workerObject.levelOfHydrated = (0.1 * workerObject.weight * workerObject.experience) + workerObject.levelOfHydrated;
-      workerObject.dizziness = false;
-      return workerObject;
-    } else {
-      return workerObject;
-    }
+  if (workerObject.dizziness == true) {
+    workerObject.levelOfHydrated = (0.1 * workerObject.weight * workerObject.experience) + workerObject.levelOfHydrated;
+    workerObject.dizziness = false;
   }
+  return workerObject;
+}
