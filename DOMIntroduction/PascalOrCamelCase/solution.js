37ae28ac5a1a966626090f2e8d9d118ed705e655
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-01-12 17:42:57 +0200

    Task solved

--- DOMIntroduction/PascalOrCamelCase/solution.js
+++ DOMIntroduction/PascalOrCamelCase/solution.js
@@ -4,23 +4,32 @@ function solve() {
   let result = document.getElementById('result');
 
   if (namingConvention === 'Camel Case') {
-    result.textContent = 'Camel Case!';
-    console.log(userInput);
+    result.textContent = toCamelCase(userInput);
   } else if (namingConvention === 'Pascal Case') {
-    result.textContent = 'Pascal Case!';
-    console.log(userInput);
+    result.textContent = toPascalCase(userInput);
   } else {
     result.textContent = 'Error!';
   }
 
-  function toPascalCase(str) {
+  function toCamelCase(str) {
+    let temp = str.replace(new RegExp(/\w+/, 'g'), (word) => {
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    });
 
-    return str;
+    temp = temp.replace(new RegExp(/\s+/, 'g'), '');
+    temp = temp.charAt(0).toLowerCase() + temp.slice(1);
+
+    return temp;
   }
 
-  function toCamelCase(str) {
+  function toPascalCase(str) {
+    let temp = str.replace(new RegExp(/\w+/, 'g'), (word) => {
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    });
 
-    return str;
+    temp = temp.replace(new RegExp(/\s+/, 'g'), '');
+
+    return temp;
   }
 
 }
