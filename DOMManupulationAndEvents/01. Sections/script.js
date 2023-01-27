803968af665e519eaeff76c50863029f1ccaa58c
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-01-27 11:33:03 +0200

    Task solved

--- DOMManupulationAndEvents/01. Sections/script.js
+++ DOMManupulationAndEvents/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   const outputArea = document.getElementById('content');
+
+   for (const word of words) {
+      const currentDiv = document.createElement('div');
+      const currentParagraph = document.createElement('p');
+
+      currentParagraph.textContent = word;
+      currentParagraph.style.display = 'none';
+      currentDiv.appendChild(currentParagraph);
+      outputArea.appendChild(currentDiv);
+
+      currentDiv.addEventListener('click', function() {
+         currentParagraph.style.display = 'block';
+      });
+   }
 }
