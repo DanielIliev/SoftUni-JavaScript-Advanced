f78dd49d0122e4c141c52780b3b8f6d8e21a0b63
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-02-02 01:33:43 +0200

    unit test done

--- UnitTesting/evenOrOdd/isOddOrEven.js
+++ UnitTesting/evenOrOdd/isOddOrEven.js
@@ -1,5 +1,11 @@
-function isOddOrEven() {
-
+function isOddOrEven(str) {
+    if (typeof (str) != 'string') {
+        return undefined;
+    } else if (str.length % 2 === 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
 }
 
 module.exports = isOddOrEven;
--- UnitTesting/evenOrOdd/isOddOrEven.test.js
+++ UnitTesting/evenOrOdd/isOddOrEven.test.js
@@ -1,1 +1,19 @@
 const { expect } = require('chai');
+const isOddOrEven = require('./isOddOrEven');
+
+describe('odd or even checker', function() {
+    it('received argument is not a string', () => {
+        const data = 1;
+        expect(isOddOrEven(data)).to.equal(undefined);
+    });
+
+    it('string length value is even', function() {
+        const data = '12';
+        expect(isOddOrEven(data)).to.equal('even');
+    });
+
+    it('string length value is odd', function() {
+        const data = '1';
+        expect(isOddOrEven(data)).to.equal('odd');
+    });
+});
