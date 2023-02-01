b1f555eeb5e0867659b47ba160b3e07ca5163c68
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-02-01 15:40:21 +0200

    task solved

---
+++ AdvancedFunctions/areaVolumeCalculator.js
@@ -0,0 +1,44 @@
+const s1 = `[
+
+    {"x":"1","y":"2","z":"10"},
+    
+    {"x":"7","y":"7","z":"10"},
+    
+    {"x":"5","y":"2","z":"10"}
+    
+    ]`;
+
+const s2 = `[
+
+    {"x":"10","y":"-22","z":"10"},
+    
+    {"x":"47","y":"7","z":"-5"},
+    
+    {"x":"55","y":"8","z":"0"},
+    
+    {"x":"100","y":"100","z":"100"},
+    
+    {"x":"55","y":"80","z":"250"}
+    
+    ]`;
+
+function area() {
+    return Math.abs(this.x * this.y);
+};
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
+function solve(area, vol, input) {
+    let shapes = JSON.parse(input);
+    let results = [];
+
+    for (const values of shapes) {
+        results.push({'area': area.apply(values), 'volume': vol.apply(values)});
+    }
+
+    return results;
+}
+
+solve(area, vol, s2);
--- DOMManupulationAndEvents/03. Locked-Profile/app.js
+++ DOMManupulationAndEvents/03. Locked-Profile/app.js
@@ -1,3 +1,15 @@
 function lockedProfile() {
-    console.log('TODO...')
+    // let test = document.getElementById('user1HiddenFields');
+    // test.style.display = 'block';
+    // console.log(window.getComputedStyle(test).display);
+
+    const showMoreButtons = Array.from(document.querySelectorAll('.profile > button'));
+
+    for (let index = 0; index < showMoreButtons.length; index++) {
+        showMoreButtons[index].setAttribute('id', index);
+
+        showMoreButtons[index].addEventListener('click', (e) => {
+            console.log(e.target.id);
+        });
+    }
 }
