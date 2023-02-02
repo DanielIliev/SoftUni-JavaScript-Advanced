fce2584619d9f48ddaa89f39642c274a419ea7ee
DanielIliev <daniel.iliev.didiencho@gmail.com>
2023-02-02 15:52:05 +0200

    task solved

--- AdvancedFunctions/functionalSum.js
+++ AdvancedFunctions/functionalSum.js
@@ -1,3 +1,16 @@
-function functionalSum() {
-    
+function add(num) {
+
+    const innerFunction = function(newNum) {
+        num += Number(newNum);
+
+        return innerFunction;
+    }
+
+    innerFunction.toString = function() {
+        return num;
+    }
+
+    return innerFunction;
 }
+
+console.log(add(3)(5)(-3).toString());
